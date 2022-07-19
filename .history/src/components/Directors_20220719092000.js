422bb0b180ecea5b098c5b32729352b4ac3704cf
peterclermy <peterclermy232@gmail.com>
2022-07-19 10:15:12 +0300

    react router

---
+++ .history/src/components/Directors_20220719092000.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { directors } from "../data";
+
+function Directors() {
+  return <div>{/*{code here}*/}</div>;
+}
+
+export default Directors;
---
+++ .history/src/components/Movies_20220719092000.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { movies } from "../data";
+
+function Movies() {
+  return <div>{/*{code here}*/}</div>;
+}
+
+export default Movies;
